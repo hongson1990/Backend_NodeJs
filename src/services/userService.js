fbcb69cc6570b0da87d1053e1420defe3d339693
Son <hongsonndvn2014@gmail.com>
2024-03-21 22:39:17 +0900

    Edit_#43

--- src/services/userService.js
+++ src/services/userService.js
@@ -88,31 +88,32 @@ let getAllUsers = (userId) => {
     })
 }
 
-let createNewUser = () => {
+let createNewUser = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
             let check = await checkUserEmail(data.email);
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already in used, Plz try another email!'
+                    errMessage: 'Your email is already in used, Plz try another email!'
                 })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
+                });
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
+                });
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId
-            });
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            });
         } catch (e) {
             reject(e);
         }
@@ -130,8 +131,74 @@ let hashUserPassword = (password) => {
     })
 }
 
+let deleteUser = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: {
+                    id: userId
+                }
+            });
+            if (!user) {
+                resolve({
+                    errCode: 2,
+                    message: `The user isn't exist`
+                });
+            }
+            await db.User.destroy({
+                where: {
+                    id: userId
+                }
+            });
+            resolve({
+                errCode: 0,
+                message: `The user is deleted`
+            });
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.id) {
+                return resolve({
+                    errCode: 2,
+                    errMessage: 'Missing required parameters'
+                });
+            }
+            let user = await db.User.findOne({
+                where: { id: data.id },
+                raw: false
+            });
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+
+                await user.save();
+                resolve({
+                    errCode: 0,
+                    message: `Update the user successds!`
+                });
+            } else {
+                resolve({
+                    errCode: 1,
+                    errMessage: `User's not found!`
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     handleUserLogin: handleUserLogin,
     getAllUsers: getAllUsers,
-    createNewUser: createNewUser
+    createNewUser: createNewUser,
+    deleteUser: deleteUser,
+    updateUserData: updateUserData
 }
